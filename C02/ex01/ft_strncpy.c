f24e498ef00d4fa486de3c877cfe257199fed366
Wenyan Lin <116374812+Iealnwnyn@users.noreply.github.com>
2023-07-24 17:35:43 +0200

    Add files via upload

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,25 +1,41 @@
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	int	i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 17:09:04 by wlin              #+#    #+#             */
+/*   Updated: 2023/07/24 17:25:58 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	i = 0;
-	while (src[i] != '\0' && i != n)
-	{
-		dest[i] = src[i]
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+char    *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int    i;
+
+    i = 0;
+    while (src[i] != '\0' && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return (dest);
 }
 
 /*
-int	main()
+int    main()
 {
-	int n = 3;
-	char src = "hola";
-	char dest = "hello";
+    //n = 3;
+    char src[10] = "hola";
+    char dest[10] = "";
 
-	ft_strncpy(dest, src, n);
-	return (0);
+    ft_strncpy(dest, src, 3);
+    return (0);
 }
 */
