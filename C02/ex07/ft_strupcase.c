de6a88ccf59b0113d197ef8a87282887c42f9519
Wenyan Lin <116374812+Iealnwnyn@users.noreply.github.com>
2023-07-25 23:54:06 +0200

    added ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 18:51:27 by wlin              #+#    #+#             */
+/*   Updated: 2023/07/25 23:25:12 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	str[50] = "hola,ciao";
+	printf("%s",ft_strupcase(str));
+	return (0);
+}*/
