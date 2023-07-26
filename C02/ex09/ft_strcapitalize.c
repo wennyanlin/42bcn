bd4b10dda76c17e7d3d65e490fa98a873a589135
Wenyan Lin <116374812+Iealnwnyn@users.noreply.github.com>
2023-07-26 22:09:13 +0200

    ex09 norm&compiler

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,35 +6,50 @@
 /*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 23:04:45 by wlin              #+#    #+#             */
-/*   Updated: 2023/07/25 23:51:08 by wlin             ###   ########.fr       */
+/*   Updated: 2023/07/26 19:40:44 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ('A' <= str[i] && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
-	if ('a' <= str[i] && str[i] <= 'z')
+	ft_strlowcase(str);
+	while (str[i] != '\0')
 	{
-		str[i] = str[i] - 32;
-	}
-	i++;
-	while (str[i])
-	{
-		if (! ('a' <= str[i] && str[i] <= 'z') 
-						|| ('A' <= str[i] && str[i] <= 'Z'))
-		i++;
-		else if ('a' <= str[i] && str[i] <= 'z')
+		if (!('A' <= str[i - 1] && str[i - 1] <= 'Z') 
+			&& !('a' <= str[i - 1] && str[i - 1] <= 'z'))
 		{
-			str[i] = str[i] - 32;
+			if ('a' <= str[i] && str[i] <= 'z')
+				str[i] = str[i] - 32;
 		}
 		i++;
 	}
+	return (str);
 }
+/*
+#include <stdio.h>
+
 int	main(void)
 {
-	char str[20] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	ft_strcapitalize(str);
+	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	printf("%s", ft_strcapitalize(str));
 	return (0);
-}
+}*/
