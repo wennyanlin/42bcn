8ba1b7aa9446163bd07635212ecdc6f412d2b310
Wenyan Lin <116374812+Iealnwnyn@users.noreply.github.com>
2023-07-27 23:58:28 +0200

    simplified

--- aff_a/aff_a.c
+++ aff_a/aff_a.c
@@ -22,8 +22,6 @@ int main(int argc, char *argv[])
 		write(1, "a\n", 2);
 		return (0);
 	}
-	if (argc == 2)
-	{
 	while (argv[1][i] != '\0')
 	{
 		if (argv[1][i] == 'a')
@@ -33,11 +31,6 @@ int main(int argc, char *argv[])
 		}
 		i++;
 	}
-	if	(argv[1][i] != 'a')
-	{
-		write(1, "\n", 1);
-		return (1);
-	}
-	}
+	write(1, "\n", 1);
 	return (0);
 }
