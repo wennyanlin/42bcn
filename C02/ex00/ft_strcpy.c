b15ecb28b0d6d99e9781d85a2d67c5614da89a26
Wenyan Lin <116374812+Iealnwnyn@users.noreply.github.com>
2023-07-27 12:39:19 +0200

    fixed error

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -3,12 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpujades <cpujades@student.42barcel>       +#+  +:+       +#+        */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:34:05 by wlin              #+#    #+#             */
-/*   Updated: 2023/07/24 18:04:25 by wlin             ###   ########.fr       */
+/*   Updated: 2023/07/27 12:35:25 by wlin             ###   ########.fr       */
 /*                                                                            */
-/*************************************************************************** */
+/* ************************************************************************** */
 
 char	*ft_strcpy(char *dest, char *src)
 {
@@ -20,14 +20,14 @@ char	*ft_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
-
 }
-/*
- int main(void)
- {
- 	char paste[5];
+#include <stdio.h>
+int	main(void)
+{
+	char paste[5];
  	char copy[5] = "hola";
-	ft_strcpy(paste, copy);
- }
- */
+	printf("%s", ft_strcpy(paste, copy));
+}
+ 
