671dca395b5ed9ad40195351d7e3eb509a8d5e58
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-09-14 13:46:19 +0200

    remove the null condition

--- curso/libft.a/ft_strlen.c
+++ curso/libft.a/ft_strlen.c
@@ -10,17 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
 	int	count;
 
 	count = 0;
-	if (!s)
-	{
-		return (count);
-	}
 	while (*s)
 	{
 		count++;
