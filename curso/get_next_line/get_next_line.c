8b8338572ad40b3737a35ade99b8de8a0339652e
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-10-11 17:10:49 +0200

    tested read function

--- curso/get_next_line/get_next_line.c
+++ curso/get_next_line/get_next_line.c
@@ -1,4 +1,16 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:06:14 by wlin              #+#    #+#             */
+/*   Updated: 2023/10/11 17:07:27 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
 
 char    *get_next_line(int fd)
 {
@@ -7,20 +19,38 @@ char    *get_next_line(int fd)
 
     if (fd < 0)
     {
-        perror('Error');
-        return (-1);
+        return (NULL);
     }
     buffer = (char *)malloc(100 * sizeof(char));
     if (buffer == NULL)
         return (NULL);
     actual_bytes_read = read(fd, buffer, 10);
-    return (0);
+	return (buffer);
+    //return (0);
+	//a function that going to read and stores every bytes read in storage
+	//a function to extract each line after read from fd and return the line
+	//a function to update storage;
 }
 
 int main()
 {
-    //open a file
-    //read the file
+    //char	*path = "/Users/wlin/Desktop/test1.txt";
+	int		f;
+	char	*str;
+
+	int i = 0;
+	fd = open("/Users/wlin/Desktop/test1.txt", O_RDONLY);
+	while (i < 3)
+	{
+		//printf("%s", str);
+			str = get_next_line(fd);
+		printf("%s", str);
+		i++;
+	}
+	free(str);
+	close(fd);
+
+	//read the file
     //print out the first 10 bytes
     //close the file
 }
---
+++ curso/get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 13:47:55 by wlin              #+#    #+#             */
+/*   Updated: 2023/10/11 14:55:35 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
