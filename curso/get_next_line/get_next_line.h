78247e50da493636de7a2ec0174762ecf5ead481
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-10-12 16:55:54 +0200

    reading buffer

---
+++ curso/get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 13:47:55 by wlin              #+#    #+#             */
+/*   Updated: 2023/10/12 16:53:45 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# define BUFFER_SIZE 2
+
+char	*get_next_line(int fd);
+
+12:\n\0
+
+#endif
