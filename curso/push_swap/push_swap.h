75bca17233c71b6a1557482da33d911134118fd6
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-11-18 01:12:19 +0100

    added function is_duplicate
    checking input integer

---
+++ curso/push_swap/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 21:33:13 by wlin              #+#    #+#             */
+/*   Updated: 2023/11/18 00:57:46 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	int				data;
+	struct s_list	*next;
+}	t_list;
+
+int	is_duplicate(int argc, char **argv);
+
+#endif
