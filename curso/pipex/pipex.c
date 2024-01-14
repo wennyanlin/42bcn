a00317ccd901d22d31d43e12dcb2f97ca877352f
Wenyan Lin <wennyan.lin@gmail.com>
2024-01-14 16:53:43 +0800

    replaced hard-coded command with command argument

--- curso/pipex/pipex.c
+++ curso/pipex/pipex.c
@@ -9,6 +9,43 @@
 		//-> 4. output the final result
 #include "pipex.h"
 
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return(i);
+}
+
+char	*string_concat(char *path, char *cmd)
+{
+	char 	*result_path;
+	size_t	path_len;
+	size_t	cmd_len;
+	size_t	total_len;
+	size_t	i;
+
+	path_len = ft_strlen(path);
+	cmd_len = ft_strlen(cmd);
+	total_len = path_len + cmd_len;
+	result_path = malloc(sizeof(char) * (total_len + 1));
+	if (!result_path)
+		return (NULL);
+	i = 0;
+	while (i < path_len)
+	{
+		result_path[i] = path[i];
+		i++;
+	}
+	cmd_len = 0;
+	while (i < total_len)
+		result_path[i++] = cmd[cmd_len++];
+	result_path[i] = '\0';
+	return (result_path);
+}
+
 char	*read_input(char *infile_name)
 	{
 		int		fd;
@@ -17,7 +54,7 @@ char	*read_input(char *infile_name)
 
 		fd = open(infile_name, O_RDWR);
 		if (fd == -1)
-			return (1);
+			return (NULL);
 		bytes_read = read(fd, buffer , 100);
 		buffer[bytes_read] = '\0';
 		printf("%s", buffer);
@@ -26,26 +63,45 @@ char	*read_input(char *infile_name)
 
 char	*execute_cmd1(char *cmd1)
 {
-	char	*args[] = {"ls", "-l", 0};
+	char	**args;
+	char	*path;
 
-
-	execve("/bin/ls", args, 0);
+	args = split(cmd1, ' ');
+	path = string_concat("/bin/", args[0]);
+	execve(path, args, 0);
 	return (0);
 }
 
+//1. replace hard-coded command with the 'cmd1'
+//pass input to cmd1
+//execute cmd2
+//output the result in the outfile
+/**
+ *
+ * - be careful with the static variable in split
+ * - figure out how to write to stdin for program/process executed by execve
+ * - figure out how to pass information between two programs/processes executed by execve
+ * - be careful with execve
+ * 	 - what happens after you call it? does your code continue after?
+ * 		- try putting a println after you call execve to execute cmd1
+ * 	 - you will need to learn a bit about processes & child processes etc
+ * - how do we know the command is!!!
+*/
+
 int	main(int argc, char **argv)
 {
-	char	*infile;
-	char	*outfile;
-	char	*cmd1;
+	// char	*infile;
+	// char	*outfile;
+	// char	*cmd1;
 	char	*cmd2;
 
 	if (argc != 5)
 		return (0);
-	infile = argv[1];
-	cmd1 = argv[2];
+	// infile = argv[1];
+	// cmd1 = argv[2];
 	cmd2 = argv[3];
-	outfile = argv[4];
-	read_input(infile);
-	execute_cmd1(cmd1);
+	// outfile = argv[4];
+	// read_input(infile);
+	// execute_cmd1(cmd1);
+	execute_cmd1(cmd2);
 }
--- curso/pipex/pipex.h
+++ curso/pipex/pipex.h
@@ -6,6 +6,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+char	*read_input(char *infile_name);
+char	*execute_cmd1();
+char	**split(char *string, char separator);
+int		count_words(char *string, char separator);
+char	*get_next_word(char *string, char separator);
+char	*string_concat(char *path, char *cmd);
+size_t  ft_strlen(char *str);
+
 
 
 #endif
+
